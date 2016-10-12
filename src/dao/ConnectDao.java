ad57dcfa41029cdc1c276dd979b23516f372c540
Xianggen <809876223@qq.com>
2016-10-12 18:56:13 +0800

    add some notes to ConnectDao

--- src/dao/ConnectDao.java
+++ src/dao/ConnectDao.java
@@ -6,8 +6,8 @@ import java.sql.DriverManager;
 public class ConnectDao {
     public static Connection getConnection() {
         String driver = "com.mysql.jdbc.Driver";
-        //String url = "jdbc:mysql://wndbjrowdlbi.rds.sae.sina.com.cn:10386/bookdb";
-        String url = "jdbc:mysql://localhost:3306/bookDB";
+        //String url = "jdbc:mysql://wndbjrowdlbi.rds.sae.sina.com.cn:10386/bookdb";    //新浪云数据库url
+        String url = "jdbc:mysql://localhost:3306/bookDB";  //本地数据库url
         String user = "root";
         String password = "root";
         Connection conn = null;
